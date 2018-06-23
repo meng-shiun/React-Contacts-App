ba3756ec49b374b5851592a16ecfd3c6c6420cb3
meng-Shiun <lin.mengshiun@gmail.com>
2018-06-23 12:53:01 +0200

    Clean state start

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import React, { Component } from 'react';
 
-class ContactList extends React.Component {
-  render() {
-
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map( person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Mikey' },
-          { name: 'Alex' },
-          { name: 'Candy' }
-        ]}/>
-
-        <ContactList contacts={[
-          { name: 'Bob' },
-          { name: 'Cindy' },
-          { name: 'Zoe' },
-          { name: 'Jack' }          
-        ]}/>
-      </div>
-    );
   }
 }
 
