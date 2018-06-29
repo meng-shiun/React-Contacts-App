ed900cac157bd5fe95ee61a2d3d44968d0e07e94
meng-Shiun <lin.mengshiun@gmail.com>
2018-06-29 16:48:53 +0200

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,14 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then(c => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([c])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
@@ -34,7 +42,14 @@ class App extends Component {
               contacts={this.state.contacts}
             />
         )}/>
-        <Route path='/create' component={CreateContact}/>
+      <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
